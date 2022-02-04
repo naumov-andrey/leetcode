31bbc905c72eb81cb644f176cc8f619307eb55cb
Naumov Andrei <naumov_a_a@outlook.com>
2022-02-04 16:27:04 +0300

    add solution to the 104. Maximum Depth of Binary Tree

---
+++ src/104. Maximum Depth of Binary Tree/104.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func max(p, q int) int {
+    if p > q {
+        return p
+    }
+    return q
+}
+
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
